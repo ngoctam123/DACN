0b4db0ac8099bc2899dd0f33b94ae1f1574ecff6
ngoctam123 <ngoctam.love1999@gmail.com>
2021-03-19 13:02:08 +0700

    Add project files.

---
+++ Managing_Teacher_Work/Controllers/GroupUserController.cs
@@ -0,0 +1,98 @@
+﻿using Managing_Teacher_Work.DAO;
+using Managing_Teacher_Work.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Managing_Teacher_Work.Controllers
+{
+    public class GroupUserController : BaseController
+    {
+        // GET: GroupUser
+        MTWDbContext db = new MTWDbContext();
+        public bool isThemMoi;
+        public ActionResult Index()
+        {
+            List<GroupUser> listGroupUser = db.GroupUser.ToList();
+            ViewBag.listGroupUser = listGroupUser;
+
+            return View();
+        }
+        //public ActionResult getList(int id)
+        //{
+
+        //    JsonSerializerSettings jss = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+        //    var hs = db.GroupUser.SingleOrDefault(x => x.ID == id);
+        //    var result = JsonConvert.SerializeObject(hs, Formatting.Indented, jss);
+        //    return this.Json(result, JsonRequestBehavior.AllowGet);
+
+
+
+        //}
+
+        public ActionResult Add(GroupUser model, string submit)
+        {
+            if (submit == "Thêm")
+            {
+
+
+                isThemMoi = true;
+                if (model != null)
+                {
+                    model.Name_GroupUser = model.Name_GroupUser.ToString().Trim();               
+                    db.GroupUser.Add(model);
+                    db.SaveChanges();
+                    model = null;
+                }
+
+                return RedirectToAction("Index");
+            }
+            else if (submit == "Cập Nhật")
+            {
+                isThemMoi = false;
+                if (model != null)
+                {
+                    var list = db.GroupUser.SingleOrDefault(x => x.ID == model.ID);
+                    list.Name_GroupUser = model.Name_GroupUser.ToString().Trim();
+                    db.SaveChanges();
+                    model = null;
+                }
+
+                return RedirectToAction("Index");
+            }
+            else if (submit == "Tìm")
+            {
+                if (!string.IsNullOrEmpty(model.Name_GroupUser))
+                {
+                    List<GroupUser> list = GetData().Where(s => s.Name_GroupUser.Contains(model.Name_GroupUser)).ToList();
+                    return View("Index", list);
+                }
+                else
+                {
+                    List<GroupUser> list = GetData();
+                    return View("Index", list);
+                }
+            }
+            else
+            {
+                List<GroupUser> list = GetData().OrderBy(s => s.Name_GroupUser).ToList();
+                return View("Index", list);
+            }
+        }
+        public List<GroupUser> GetData()
+        {
+            return db.GroupUser.ToList();
+
+
+        }
+        public ActionResult Delete(int id)
+        {
+            new GroupUserDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Managing_Teacher_Work/DAO/UserDao.cs
@@ -0,0 +1,117 @@
+﻿using Managing_Teacher_Work.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Managing_Teacher_Work.DAO
+{
+    public class UserDao
+    {
+        MTWDbContext db = null;
+
+        public UserDao()
+        {
+            db = new MTWDbContext();
+        }
+        public IEnumerable<User> Listpg(int page, int pageSize)
+        {
+            return db.User.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+        public List<User> ListAll()
+        {
+            return db.User.ToList();
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.User.Find(id);
+                db.User.Remove(user);
+                db.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        public User GetById(string userName)
+        {
+            return db.User.SingleOrDefault(x => x.UserName == userName);
+        }
+        public List<string> GetListCredential(string userName)
+        {
+            var user = db.User.Single(x => x.UserName == userName);
+            var data = (from a in db.Credentials
+                        join b in db.GroupUser on a.UserGroupID equals b.ID
+                        join c in db.Role on a.RoleID equals c.ID
+                        where b.ID == user.GroupID
+                        select new
+                        {
+                            RoleID = a.RoleID,
+                            UserGroupID = a.UserGroupID
+                        }).AsEnumerable().Select(x => new Credentials()
+                        {
+                            RoleID = x.RoleID,
+                            UserGroupID = x.UserGroupID
+                        });
+            return data.Select(x => x.RoleID).ToList();
+
+        }
+        public User Get(string username)
+        {
+            return db.User.SingleOrDefault(x => x.UserName == username);
+        }
+        public int Login(string username, string password, bool isLoginAdmin = false)
+        {
+            var result = db.User.SingleOrDefault(x => x.UserName == username);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (isLoginAdmin == true)
+                {
+                    if (result.GroupID == Managing_Teacher_Work.CommonConstants.ADMIN_GROUP|| result.GroupID == Managing_Teacher_Work.CommonConstants.MEMBER_GROUP)
+                    {
+                        if (result.Status == false)
+                        {
+                            return -1;
+                        }
+                        else
+                        {
+                            if (result.Password == password)
+                                return 1;
+                            else
+                                return -2;
+                        }
+                    }
+                    else
+                    {
+                        return -3;
+                    }
+                }
+                else
+                {
+                    if (result.Status == false)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        if (result.Password == password)
+                            return 1;
+                        else
+                            return -2;
+                    }
+                }
+            }
+
+        }
+    }
+}
